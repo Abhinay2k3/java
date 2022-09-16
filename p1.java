2a8a20f8ac8459efd27e09f63e07a2e3e3c37b55
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-09-15 20:58:46 -0700

    Update p1.java

--- p1.java
+++ p1.java
@@ -13,13 +13,19 @@ class rec {
     }
 }
 class p1{
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int l = sc.nextInt();
         int w = sc.nextInt();
-        rec rec1=new rec();
-        rec1.set(l,w);
-        System.out.println("area=" + rec1.area());
-        System.out.println("perimeter" + rec1.perimeter());
+        if (l < 0) {
+            System.out.println("rectangle is not formed");
+        } else if (w < 0) {
+            System.out.println("rectangle not formed");
+        } else {
+            rec rec1 = new rec();
+            rec1.set(l, w);
+            System.out.println("area=" + rec1.area());
+            System.out.println("perimeter" + rec1.perimeter());
+        }
     }
     }
