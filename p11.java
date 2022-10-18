25bbc9120b6f1e79540440793265873ec8b5c0cb
balivada abhinay <94745716+Abhinay2k3@users.noreply.github.com>
2022-10-18 15:42:44 +0530

    Update and rename pack2.java to p11.java

---
+++ p11.java
@@ -0,0 +1,9 @@
+import p1.p10;
+public class p11
+{
+public static void main(String args[])
+{
+p10 a = new p10();
+a.display();
+}
+}
--- pack2.java
+++
@@ -1,9 +0,0 @@
-import p1.parentclass;
-class p11
-{
-public static void main(String args[])
-{
-parentClass a new parentClass();
-a.display();
-}
-}
